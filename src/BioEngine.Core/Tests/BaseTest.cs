cd680e6f0784e729438bc4084b2023566c143f73
SonicGD <sonicgd@gmail.com>
2018-10-15 17:56:02 +0500

    Rewrite base test to use DI

--- src/BioEngine.Core/Tests/BaseTest.cs
+++ src/BioEngine.Core/Tests/BaseTest.cs
@@ -1,122 +1,162 @@
 using System;
+using System.Collections.Generic;
 using BioEngine.Core.DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Npgsql;
-using Xunit;
 using Xunit.Abstractions;
 
 namespace BioEngine.Core.Tests
 {
     public abstract class BaseTest
     {
-        protected readonly ILoggerFactory LoggerFactory;
-        protected readonly ILogger Logger;
+        protected ITestOutputHelper TestOutputHelper { get; }
 
         protected BaseTest(ITestOutputHelper testOutputHelper)
         {
-            LoggerFactory = new LoggerFactory();
-            LoggerFactory.AddProvider(new XunitLoggerProvider(testOutputHelper));
-            Logger = LoggerFactory.CreateLogger<BaseTest>();
+            TestOutputHelper = testOutputHelper;
         }
     }
 
-    public abstract class BaseTest<T> : BaseTest, IClassFixture<T>, IDisposable where T : BaseTestFixture
+    public abstract class BaseTest<T> : BaseTest, IDisposable where T : BaseTestScope
     {
-        private readonly BaseTestFixture _testFixture;
-
-
-        protected BaseTest(BaseTestFixture testFixture, ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        protected BaseTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
         {
-            _testFixture = testFixture;
         }
 
-        protected BioContext GetDbContext(string databaseName, bool inMemory = true, bool init = true)
+        private readonly Dictionary<string, BaseTestScope> _scopes = new Dictionary<string, BaseTestScope>();
+
+        private BaseTestScope GetScope(string name)
         {
-            return _testFixture.GetDbContext(LoggerFactory, databaseName, inMemory, init);
+            if (!_scopes.ContainsKey(name))
+            {
+                var scope = Activator.CreateInstance<T>();
+                scope.Configure(name, TestOutputHelper);
+                _scopes.Add(name, scope);
+            }
+
+            return _scopes[name];
+        }
+
+        protected BioContext GetDbContext(string name, bool init = true)
+        {
+            return GetScope(name).GetDbContext(init);
         }
 
         public void Dispose()
         {
-            LoggerFactory?.Dispose();
+            foreach (var testScope in _scopes)
+            {
+                testScope.Value.Dispose();
+            }
         }
     }
 
-    public abstract class BaseTestFixture
+    public abstract class BaseTestScope : IDisposable
     {
-        public BaseTestFixture()
+        public void Configure(string dbName, ITestOutputHelper testOutputHelper)
         {
-            Configuration = new ConfigurationBuilder()
+            _configuration = new ConfigurationBuilder()
                 .AddUserSecrets("bw")
                 .AddEnvironmentVariables()
                 .Build();
-        }
-
-        public BioContext GetDbContext(ILoggerFactory loggerFactory, string databaseName = "",
-            bool inMemory = true, bool needInit = true)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<BioContext>()
-                .EnableSensitiveDataLogging()
-                .UseLoggerFactory(loggerFactory);
-            DbContextOptions<BioContext> options;
-            if (inMemory)
+            var services = new ServiceCollection();
+            services.AddLogging(o => o.AddProvider(new XunitLoggerProvider(testOutputHelper)));
+            ConfigureServices(services);
+            bool.TryParse(_configuration["BE_TESTS_POSTGRES"], out var testWithPostgres);
+            if (testWithPostgres)
             {
-                options =
-                    optionsBuilder.ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
-                        .UseInMemoryDatabase(databaseName).Options;
+                services.AddEntityFrameworkNpgsql();
             }
             else
             {
-                options = optionsBuilder
-                    .UseNpgsql(GetConnectionString(databaseName))
-                    .ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning))
-                    .Options;
+                services.AddEntityFrameworkInMemoryDatabase();
             }
 
-            ModifyDbOptions(options, inMemory);
-            var dbContext = new BioContext(options);
-            if (dbContext == null)
+            services.AddDbContext<BioContext>((p, optionsBuilder) =>
             {
-                throw new Exception("Can't create db context");
-            }
+                optionsBuilder.EnableSensitiveDataLogging().UseInternalServiceProvider(p);
 
-            dbContext.Database.EnsureDeleted();
-            dbContext.Database.EnsureCreated();
-            if (needInit)
+                if (testWithPostgres)
+                {
+                    optionsBuilder
+                        .UseNpgsql(GetConnectionString(dbName))
+                        .ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
+                }
+                else
+                {
+                    optionsBuilder.ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                        .UseInMemoryDatabase(dbName);
+                }
+
+                ModifyDbOptions(optionsBuilder.Options, !testWithPostgres);
+            });
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected virtual IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            return services;
+        }
+
+        private BioContext _bioContext;
+
+        public BioContext GetDbContext(bool needInit)
+        {
+            if (_bioContext == null)
             {
-                InitDbContext(dbContext);
+                _bioContext = _serviceProvider.GetService<BioContext>();
+                if (_bioContext == null)
+                {
+                    throw new Exception("Can't create db context");
+                }
+
+                _bioContext.Database.EnsureDeleted();
+                _bioContext.Database.EnsureCreated();
+                if (needInit)
+                {
+                    InitDbContext(_bioContext);
+                }
             }
 
-            return dbContext;
+            return _bioContext;
         }
 
         protected virtual string GetConnectionString(string databaseName)
         {
             var connBuilder = new NpgsqlConnectionStringBuilder
             {
-                Host = Configuration["BIO_POSTGRES_HOST"] ?? "localhost",
+                Host = _configuration["BIO_POSTGRES_HOST"] ?? "localhost",
                 Port =
-                    !string.IsNullOrEmpty(Configuration["BIO_POSTGRES_PORT"])
-                        ? int.Parse(Configuration["BIO_POSTGRES_PORT"])
+                    !string.IsNullOrEmpty(_configuration["BIO_POSTGRES_PORT"])
+                        ? int.Parse(_configuration["BIO_POSTGRES_PORT"])
                         : 5432,
-                Username = Configuration["BIO_POSTGRES_USERNAME"] ?? "postgres",
-                Password = Configuration["BIO_POSTGRES_PASSWORD"] ?? "",
+                Username = _configuration["BIO_POSTGRES_USERNAME"] ?? "postgres",
+                Password = _configuration["BIO_POSTGRES_PASSWORD"] ?? "",
                 Database = databaseName,
                 Pooling = false
             };
             return connBuilder.ConnectionString;
         }
 
-        public readonly IConfiguration Configuration;
+        private IConfiguration _configuration;
+        private IServiceProvider _serviceProvider;
 
         protected virtual void InitDbContext(BioContext dbContext)
         {
         }
 
-        protected virtual void ModifyDbOptions(DbContextOptions<BioContext> options, bool inMemory)
+        protected virtual void ModifyDbOptions(DbContextOptions options, bool inMemory)
         {
+        }
+
+        public void Dispose()
+        {
+            _bioContext.Database.EnsureDeleted();
+            _bioContext.Dispose();
         }
     }
 }
