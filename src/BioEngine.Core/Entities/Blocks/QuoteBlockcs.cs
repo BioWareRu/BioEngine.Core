ffaa8ef3e289f1a097691abffa471b73bf689e17
George <sonicgd@gmail.com>
2019-04-01 17:11:35 +0500

    New blocks!

---
+++ src/BioEngine.Core/Entities/Blocks/QuoteBlockcs.cs
@@ -0,0 +1,19 @@
+﻿namespace BioEngine.Core.Entities.Blocks
+{
+    public class QuoteBlock : ContentBlock<QuoteBlockData>
+    {
+        public override string TypeTitle { get; set; } = "Цитата";
+
+        public override string ToString()
+        {
+            return $"{Data.Author}: {Data.Text} ({Data.Link})";
+        }
+    }
+
+    public class QuoteBlockData : ContentBlockData
+    {
+        public string Text { get; set; }
+        public string Author { get; set; }
+        public string Link { get; set; }
+    }
+}
