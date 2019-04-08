b5adbc3e535d4d889ef0d6b5264c941899f338bd
George <sonicgd@gmail.com>
2019-04-08 22:44:28 +0500

    Add attribute to test class

--- tests/BioEngine.Core.Tests/Fixtures/TestSection.cs
+++ tests/BioEngine.Core.Tests/Fixtures/TestSection.cs
@@ -1,7 +1,9 @@
+using BioEngine.Core.DB;
 using BioEngine.Core.Entities;
 
 namespace BioEngine.Core.Tests.Fixtures
 {
+    [TypedEntity("testsection")]
     public class TestSection : Section<TestSectionData>
     {
         public override string TypeTitle { get; set; } = "Раздел";
