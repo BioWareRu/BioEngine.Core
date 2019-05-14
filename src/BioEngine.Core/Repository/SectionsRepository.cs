b23b967fb74a1dfec99a1ceec0a87706239d3799
George <sonicgd@gmail.com>
2019-05-14 11:30:42 +0500

    Update posts siteids after section sites changed

--- src/BioEngine.Core/Repository/SectionsRepository.cs
+++ src/BioEngine.Core/Repository/SectionsRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using BioEngine.Core.DB;
 using BioEngine.Core.Entities;
 using JetBrains.Annotations;
@@ -16,13 +17,42 @@ namespace BioEngine.Core.Repository
 
     public abstract class SectionRepository<T> : SiteEntityRepository<T> where T : Section
     {
-        public SectionRepository(BioRepositoryContext<T> repositoryContext) : base(repositoryContext)
+        protected SectionRepository(BioRepositoryContext<T> repositoryContext) : base(repositoryContext)
         {
         }
 
         protected override IQueryable<T> GetBaseQuery(QueryContext<T>? queryContext = null)
         {
             return ApplyContext(DbContext.Set<T>().Include(p => p.Blocks), queryContext);
+        }
+
+        protected override async Task<bool> AfterSaveAsync(T item, PropertyChange[] changes = null,
+            T oldItem = null,
+            IBioRepositoryOperationContext operationContext = null)
+        {
+            var res = await base.AfterSaveAsync(item, changes, oldItem, operationContext);
+            if (res && changes != null && changes.Any(c => c.Name == nameof(Section.SiteIds)))
+            {
+                var hasChanges = false;
+                var posts = await DbContext.Posts.Where(p => p.SectionIds.Contains(item.Id)).ToArrayAsync();
+                foreach (var post in posts)
+                {
+                    var sections = await DbContext.Sections.Where(s => post.SectionIds.Contains(s.Id)).ToArrayAsync();
+                    post.SiteIds = sections.SelectMany(s => s.SiteIds).Distinct().ToArray();
+                    if (post.SiteIds.Any())
+                    {
+                        DbContext.Update(post);
+                        hasChanges = true;
+                    }
+                }
+
+                if (hasChanges)
+                {
+                    await DbContext.SaveChangesAsync();
+                }
+            }
+
+            return res;
         }
     }
 }
