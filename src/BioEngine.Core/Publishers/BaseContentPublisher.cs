53f2e568f88aa7c0058298a3b5f523886b619215
George <sonicgd@gmail.com>
2019-05-16 16:45:12 +0500

    Separate content publishers fom repository hooks

---
+++ src/BioEngine.Core/Publishers/BaseContentPublisher.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BioEngine.Core.DB;
+using BioEngine.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BioEngine.Core.Publishers
+{
+    public abstract class BaseContentPublisher<TConfig, TPublishRecord> : IContentPublisher<TConfig>
+        where TConfig : IContentPublisherConfig where TPublishRecord : BasePublishRecord
+    {
+        protected ILogger<IContentPublisher<TConfig>> Logger { get; }
+        private readonly BioContext _dbContext;
+
+        protected BaseContentPublisher(BioContext dbContext, ILogger<IContentPublisher<TConfig>> logger)
+        {
+            Logger = logger;
+            _dbContext = dbContext;
+        }
+
+        public virtual async Task<bool> PublishAsync(IContentEntity entity, Site site, TConfig config)
+        {
+            try
+            {
+                var record = await DoPublishAsync(entity, site, config);
+                _dbContext.Add(record);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "Error while publishing to {publisher}: {errorText}", GetType().FullName,
+                    ex.ToString());
+                return false;
+            }
+        }
+
+        public virtual async Task<bool> DeleteAsync(IContentEntity entity, TConfig config, Site site = null)
+        {
+            var records = (await GetRecordsAsync(entity)).ToArray();
+            if (!records.Any())
+            {
+                return false;
+            }
+
+            foreach (var record in records)
+            {
+                if (site != null && record.SiteId != site.Id)
+                {
+                    continue;
+                }
+
+                var deleted = await DoDeleteAsync(record, config);
+                if (!deleted)
+                {
+                    Logger.LogError("Can't delete content from {publisher}", GetType().FullName);
+                    return false;
+                }
+
+                _dbContext.Remove(record);
+            }
+
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        protected async Task<TPublishRecord> GetRecordAsync(IContentEntity entity, Site site = null)
+        {
+            return await _dbContext.Set<TPublishRecord>()
+                .FirstOrDefaultAsync(r =>
+                    r.Type == entity.GetType().FullName && r.ContentId == entity.Id &&
+                    (site == null || r.SiteId == site.Id));
+        }
+
+        protected async Task<IEnumerable<TPublishRecord>> GetRecordsAsync(IContentEntity entity)
+        {
+            return await _dbContext.Set<TPublishRecord>()
+                .Where(r =>
+                    r.Type == entity.GetType().FullName && r.ContentId == entity.Id)
+                .ToListAsync();
+        }
+
+        protected abstract Task<TPublishRecord> DoPublishAsync(IContentEntity entity, Site site, TConfig config);
+        protected abstract Task<bool> DoDeleteAsync(TPublishRecord record, TConfig config);
+    }
+}
---
+++ src/BioEngine.Core/Publishers/IContentPublisher.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+using BioEngine.Core.Entities;
+
+namespace BioEngine.Core.Publishers
+{
+    public interface IContentPublisher<TConfig> where TConfig : IContentPublisherConfig
+    {
+        Task<bool> PublishAsync(IContentEntity entity, Site site, TConfig config);
+        Task<bool> DeleteAsync(IContentEntity entity, TConfig config, Site site = null);
+    }
+
+    public interface IContentPublisherConfig
+    {
+    }
+
+    public abstract class BasePublishRecord : BaseSingleSiteEntity
+    {
+        [NotMapped] public override string Title { get; set; }
+        [NotMapped] public override string Url { get; set; }
+        public Guid ContentId { get; set; }
+        public string Type { get; set; }
+    }
+}
