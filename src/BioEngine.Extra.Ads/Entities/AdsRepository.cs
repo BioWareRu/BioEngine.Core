d92581898642f93ea10e0c71890d9d16da0a7e97
George <sonicgd@gmail.com>
2019-05-16 17:13:42 +0500

    Implement main site selection via policy

--- src/BioEngine.Extra.Ads/Entities/AdsRepository.cs
+++ src/BioEngine.Extra.Ads/Entities/AdsRepository.cs
@@ -4,7 +4,8 @@ namespace BioEngine.Extra.Ads.Entities
 {
     public class AdsRepository : SiteEntityRepository<Ad>
     {
-        public AdsRepository(BioRepositoryContext<Ad> repositoryContext) : base(repositoryContext)
+        public AdsRepository(BioRepositoryContext<Ad> repositoryContext,
+            IMainSiteSelectionPolicy mainSiteSelectionPolicy) : base(repositoryContext, mainSiteSelectionPolicy)
         {
         }
     }
