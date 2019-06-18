ff245d6e38c4ed324ad40bff8fd3451a69716161
George <sonicgd@gmail.com>
2019-06-18 18:02:38 +0500

    Fix imports

--- src/BioEngine.Extra.Ads/Site/AdsProvider.cs
+++ src/BioEngine.Extra.Ads/Site/AdsProvider.cs
@@ -1,8 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using BioEngine.Core.Extensions;
+using BioEngine.Core.Repository;
 using BioEngine.Extra.Ads.Entities;
 
 namespace BioEngine.Extra.Ads.Site
