cfb199017fc499dad14f611b01d26e866e08cb35
George <sonicgd@gmail.com>
2019-06-19 10:29:49 +0500

    Auto set site id in ISiteEntity if siteIds is empty and there is only 1 site in db

--- src/BioEngine.Core/Repository/SiteEntityRepository.cs
+++ src/BioEngine.Core/Repository/SiteEntityRepository.cs
@@ -1,5 +1,10 @@
-﻿using BioEngine.Core.Abstractions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BioEngine.Core.Abstractions;
 using BioEngine.Core.Validation;
+using FluentValidation.Results;
+using Microsoft.EntityFrameworkCore;
 
 namespace BioEngine.Core.Repository
 {
@@ -8,6 +13,22 @@ namespace BioEngine.Core.Repository
     {
         protected SiteEntityRepository(BioRepositoryContext<TEntity> repositoryContext) : base(repositoryContext)
         {
+        }
+
+        protected override async Task<bool> BeforeValidateAsync(TEntity item,
+            (bool isValid, IList<ValidationFailure> errors) validationResult, PropertyChange[] changes = null,
+            IBioRepositoryOperationContext operationContext = null)
+        {
+            if (item.SiteIds.Length == 0)
+            {
+                var sites = await DbContext.Sites.ToListAsync();
+                if (sites.Count == 1)
+                {
+                    item.SiteIds = new[] {sites.First().Id};
+                }
+            }
+
+            return await base.BeforeValidateAsync(item, validationResult, changes, operationContext);
         }
 
         protected override void RegisterValidators()
