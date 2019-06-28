04209975a819f10fa2595e51a581d642afd4a9ab
George <sonicgd@gmail.com>
2019-06-28 11:21:13 +0500

    Drop old property

--- src/BioEngine.Extra.Ads/Api/Entities/Ad.cs
+++ src/BioEngine.Extra.Ads/Api/Entities/Ad.cs
@@ -1,11 +1,7 @@
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using BioEngine.Core.Api.Models;
-using BioEngine.Core.Entities;
 using BioEngine.Core.Repository;
 using Microsoft.AspNetCore.Routing;
-using ContentBlock = BioEngine.Core.Api.Entities.ContentBlock;
 
 namespace BioEngine.Extra.Ads.Api.Entities
 {
@@ -13,8 +9,6 @@ namespace BioEngine.Extra.Ads.Api.Entities
     public class Ad : ContentEntityRestModel<Ads.Entities.Ad>, IContentRequestRestModel<Ads.Entities.Ad>,
         IContentResponseRestModel<Ads.Entities.Ad>
     {
-        [Required] public StorageItem Picture { get; set; }
-
         public async Task<Ads.Entities.Ad> GetEntityAsync(Ads.Entities.Ad entity)
         {
             entity = await FillEntityAsync(entity);
@@ -25,8 +19,6 @@ namespace BioEngine.Extra.Ads.Api.Entities
         {
             await ParseEntityAsync(entity);
         }
-
-        public List<ContentBlock> Blocks { get; set; }
 
         public Ad(LinkGenerator linkGenerator, SitesRepository sitesRepository) : base(linkGenerator, sitesRepository)
         {
