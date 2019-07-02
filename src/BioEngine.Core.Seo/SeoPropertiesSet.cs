b17aff55fdfcfb1cfc00fdf8fce960dd1e09b82f
George <sonicgd@gmail.com>
2019-07-02 11:22:39 +0500

    Add Google TagManager support

--- src/BioEngine.Core.Seo/SeoPropertiesSet.cs
+++ src/BioEngine.Core.Seo/SeoPropertiesSet.cs
@@ -8,7 +8,9 @@ namespace BioEngine.Core.Seo
         [PropertiesElement("Описание", PropertyElementType.LongString)]
         public string Description { get; set; } = "";
 
-        [PropertiesElement("Ключевые слова")]
-        public string Keywords { get; set; } = "";
+        [PropertiesElement("Ключевые слова")] public string Keywords { get; set; } = "";
+
+        [PropertiesElement("Google TagManager Id")]
+        public string TagManagerId { get; set; } = "";
     }
 }
