8a665511511acc8a3d607542c732e046b4686693
George <sonicgd@gmail.com>
2019-07-05 19:04:20 +0500

    fix

--- src/BioEngine.Extra.Ads/AdsModule.cs
+++ src/BioEngine.Extra.Ads/AdsModule.cs
@@ -10,7 +10,7 @@ namespace BioEngine.Extra.Ads
         public override void ConfigureEntities(IServiceCollection serviceCollection, BioEntitiesManager entitiesManager)
         {
             base.ConfigureEntities(serviceCollection, entitiesManager);
-            RegisterRepositories(typeof(AdsModule).Assembly, serviceCollection, entitiesManager);
+            RegisterRepositories(typeof(Ad).Assembly, serviceCollection, entitiesManager);
             entitiesManager.ConfigureDbContext(modelBuilder =>
             {
                 modelBuilder.Entity<Ad>().HasMany(contentItem => contentItem.Blocks).WithOne()
