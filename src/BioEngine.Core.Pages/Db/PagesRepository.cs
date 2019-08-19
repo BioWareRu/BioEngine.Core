73cbf26cf7659eb076effbcd9976eed728b52390
George <sonicgd@gmail.com>
2019-08-19 10:33:43 +0500

    Drop include

--- src/BioEngine.Core.Pages/Db/PagesRepository.cs
+++ src/BioEngine.Core.Pages/Db/PagesRepository.cs
@@ -1,17 +1,12 @@
-﻿using System.Linq;
-using BioEngine.Core.Pages.Entities;
+﻿using BioEngine.Core.Pages.Entities;
 using BioEngine.Core.Repository;
-using Microsoft.EntityFrameworkCore;
+using JetBrains.Annotations;
 
 namespace BioEngine.Core.Pages.Db
 {
+    [UsedImplicitly]
     public class PagesRepository : ContentEntityRepository<Page>
     {
-        protected override IQueryable<Page> AddIncludes(IQueryable<Page> query)
-        {
-            return base.AddIncludes(query).Include(p => p.Blocks);
-        }
-
         public PagesRepository(BioRepositoryContext<Page> repositoryContext) : base(repositoryContext)
         {
         }
