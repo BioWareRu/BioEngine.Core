281ac2862f0c72fdbe5fe55e9cbc893e5c8bb9b5
George <sonicgd@gmail.com>
2019-11-18 15:09:55 +0500

    Make each repository run on own scope

--- src/BioEngine.Core/Repository/BioRepositoryContext.cs
+++ src/BioEngine.Core/Repository/BioRepositoryContext.cs
@@ -1,29 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using BioEngine.Core.Abstractions;
 using BioEngine.Core.DB;
 using BioEngine.Core.Properties;
 using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BioEngine.Core.Repository
 {
-    public class BioRepositoryContext<T> where T : class, IEntity
+    public class BioRepositoryContext<T> : IDisposable where T : class, IEntity
     {
+        private readonly IServiceScope _scope;
         internal BioContext DbContext { get; }
         public List<IValidator<T>>? Validators { get; }
         public PropertiesProvider PropertiesProvider { get; }
         public BioRepositoryHooksManager HooksManager { get; }
 
         public BioRepositoryContext(
-            BioContext dbContext,
-            PropertiesProvider propertiesProvider,
-            BioRepositoryHooksManager hooksManager,
-            IEnumerable<IValidator<T>>? validators = default)
+            IServiceScopeFactory serviceScopeFactory)
         {
-            DbContext = dbContext;
-            PropertiesProvider = propertiesProvider;
-            HooksManager = hooksManager;
-            Validators = validators?.ToList();
+            _scope = serviceScopeFactory.CreateScope();
+            DbContext = _scope.ServiceProvider.GetRequiredService<BioContext>();
+            PropertiesProvider = _scope.ServiceProvider.GetRequiredService<PropertiesProvider>();
+            HooksManager = _scope.ServiceProvider.GetRequiredService<BioRepositoryHooksManager>();
+            Validators = _scope.ServiceProvider.GetServices<IValidator<T>>()?.ToList() ?? new List<IValidator<T>>();
+        }
+
+        public void Dispose()
+        {
+            _scope.Dispose();
         }
     }
 }
